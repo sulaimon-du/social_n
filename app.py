f7f65d1e9f28ff5deadd6c86f393e48b50215757
sulaimon <sulaimon.gh@gmail.com>
2025-09-15 23:48:22 +0500

    Added: Permissions check and redirect, validation check. Changed: Error output and minor things.

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from werkzeug.security import generate_password_hash, check_password_hash
 import sqlite3
+from future.is_allowed import validate_username, validate_email, validate_login
+
 
 app = Flask(__name__)
 app.secret_key = "secret_key_5051"
@@ -9,6 +11,7 @@ def get_db_connection():
     conn = sqlite3.connect('app.db')
     conn.row_factory = sqlite3.Row
     return conn
+
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
@@ -28,17 +31,19 @@ def index():
 
 @app.route("/post/add", methods=['GET','POST'])
 def add_post():
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
     if request.method == "POST":
-        user_id = session['user_id']
-        title = request.form['title']
-        content = request.form['content']
-        conn = get_db_connection()
-        conn.execute("INSERT INTO posts (user_id, title, content) VALUES (?, ?, ?)", (user_id, title, content))
-        conn.commit()
-        post_id = conn.execute("SELECT id FROM posts WHERE user_id = ? AND title = ?", (user_id, title)).fetchone()
-        post_id = post_id[0]
-        conn.close()
-        return redirect(url_for('get_post', post_id=post_id))
+            user_id = session['user_id']
+            title = request.form['title']
+            content = request.form['content']
+            conn = get_db_connection()
+            conn.execute("INSERT INTO posts (user_id, title, content) VALUES (?, ?, ?)", (user_id, title, content))
+            conn.commit()
+            post_id = conn.execute("SELECT id FROM posts WHERE user_id = ? AND title = ?", (user_id, title)).fetchone()
+            post_id = post_id[0]
+            conn.close()
+            return redirect(url_for('get_post', post_id=post_id))
     return render_template("post.html")
 
 
@@ -52,52 +57,80 @@ def get_post(post_id):
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
+    if 'user_id' in session:
+     return redirect(url_for("profile"))
+    
     if request.method == 'POST':
         username = request.form['username']
         email = request.form['email']
         password = request.form['password']
+        validated_u = validate_username(username)
+        validated_e = validate_email(email)
+
+        if not username or not email or not password or validated_u or validated_e:
+            error = "Пожалуйста заполните все поля"
+            if validated_u and validated_e:
+                error = f"{validated_u} и {validated_e}"
+            elif validated_e or validated_u:
+                error = f"{validated_u}{validated_e}"
+            
+            return render_template("register.html", error=error)
+            
+
         hash_pw = generate_password_hash(password)
 
         conn = get_db_connection()
         try:
-            conn.execute("INSERT INTO users (username, password, email) VALUES (?, ?, ?)", (username, hash_pw, email))
+            conn.execute("INSERT INTO users (username, email, password) VALUES (?, ?, ?) ", (username, email, hash_pw))
             conn.commit()
         except:
-            return "Ошибка: такой пользователь уже существует!"
+            error = "Ошибка: пользователь c таким именем или почтой уже существует!"
+            return render_template("register.html", error = error)
         finally:
             conn.close()
-
-        return redirect('/login')
+        return redirect(url_for("login"))
+        
     return render_template("register.html")
+
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    if 'user_id' in session:
+        return redirect(url_for('profile'))
+    
+    error = None
+
     if request.method == 'POST':
-        username = request.form['username']
+        login = request.form['login']
         password = request.form['password']
-        
+        validated = validate_login(login)
+        if validated:
+            error = f"Неверное имя пользователя или email - ({login})"
+            return render_template("login.html", error=error)
         conn = get_db_connection()
-        user = conn.execute("SELECT * FROM users WHERE username = ?", (username,)).fetchone()
+        user = conn.execute("SELECT * FROM users WHERE username = ? OR email = ?", (login, login)).fetchone()
         conn.close()
+        
 
         if user and check_password_hash(user['password'], password):
-            session["user_id"] = user['id']
-            session["username"] = user["username"]
-            session["email"] = user["email"]
-            return redirect("/")
-        return "Неверное имя пользователя или пароль!"
-    
-    return render_template("login.html")
+            session['user_id'] = user['id']
+            session['username'] = user['username']
+            session['email'] = user['email']
+            return redirect(url_for('profile'))
+        else:
+            error = "Неверное имя пользователя/email или пароль!"
+    return render_template("login.html", error=error)
 
 
 @app.route("/logout")
 def logout():
     session.clear()
-    return redirect("/")
-
+    return redirect(url_for("index"))
 
 @app.route("/profile")
 def profile():
+    if 'user_id' not in session:
+        return redirect('login')
     return render_template("profile.html")
 
     
